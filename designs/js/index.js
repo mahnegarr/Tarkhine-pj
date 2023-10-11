d9a7af8b3dbe14f8b7b0a3f7c62fc06bd9032ed2
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-10-11 19:23:46 +0330

    #

--- designs/js/index.js
+++ designs/js/index.js
@@ -1,7 +1,51 @@
 const logout = document.getElementById("logout");
-const backdrop = document.querySelector(".backdrop")
+const backdrop = document.querySelector(".backdrop");
+const closeIcon = document.querySelector(".close-modal");
+const modal = document.querySelector(".modal");
+const phoneNumber = document.getElementById("number");
+const continueBtn = document.getElementById("continue");
 
-logout.addEventListener("click",()=>{
-    backdrop.style.display="block"
+logout.addEventListener("click", () => {
+  backdrop.style.display = "block";
+});
+function closeModal() {
+  modal.style.display = "none";
+  backdrop.style.display = "none";
+}
+closeIcon.addEventListener("click", () => {
+  closeModal();
+});
+
+window.addEventListener("click",()=>{
+    closeModal()
 })
 
+function validateNumber(phoneNumber) {
+  if (phoneNumber.length !== 11) {
+    alert("تعداد رقم ها کافی نیست");
+    phoneNumber.value = "";
+    continueBtn.disabled = true; // disable the button
+    continueBtn.classList.remove("enable-btn"); // remove the class
+    return false; // return false to indicate validation failed
+  }
+  if (phoneNumber.charAt(0) !== "0" || phoneNumber.charAt(1) !== "9") {
+    alert("شماره باید با 09 شروع شود");
+    phoneNumber.value = "";
+    continueBtn.disabled = true; // disable the button
+    continueBtn.classList.remove("enable-btn"); // remove the class
+    return false; // return false to indicate validation failed
+  }
+  localStorage.setItem("phoneNumber", phoneNumber);
+  enableBtn(); // call the enableButton function if validation succeeded
+  return true; // return true to indicate validation succeeded
+}
+let number = "";
+phoneNumber.addEventListener("focusout", (e) => {
+  number = e.target.value;
+  validateNumber(number);
+});
+
+function enableBtn() {
+  continueBtn.disabled = false;
+  continueBtn.classList.add("enable-btn");
+}
