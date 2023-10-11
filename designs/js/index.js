5d16c0d9582c858343720183b98d2d35ab72335a
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-10-11 10:43:32 +0330

    #

---
+++ designs/js/index.js
@@ -0,0 +1,7 @@
+const logout = document.getElementById("logout");
+const backdrop = document.querySelector(".backdrop")
+
+logout.addEventListener("click",()=>{
+    backdrop.style.display="block"
+})
+
