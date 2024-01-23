1fb5082aa4ee7b31682abf5abbff03f725f8111a
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2024-01-23 13:54:10 +0330

    #

--- designs/js/index.js
+++ designs/js/index.js
@@ -17,7 +17,7 @@ function closeModal() {
   localStorage.removeItem("modalShown");
 }
 login.addEventListener("click", () => {
-  showModal()
+  showModal();
 });
 closeIcon.addEventListener("click", () => {
   closeModal();
@@ -58,5 +58,38 @@ function enableBtn() {
 }
 
 searchBtn.addEventListener("click", () => {
-  showModal()
+  showModal();
 });
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+const plusSlides = (n) => {
+  showSlides((slideIndex += n));
+};
+
+const currentSlides = (n) => {
+  showSlides((slideIndex = n));
+};
+
+const showSlides = (n) => {
+  var i;
+  var slides = document.getElementsByClassName("slides fade");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += "active";
+};
